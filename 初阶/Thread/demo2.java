ed1b31d7fc38e817c357c3672b42faa5980dbcf6
dcy <2284858868@qq.com>
2024-03-23 22:02:32 +0800

    线程创建方法2

---
+++ 初阶/Thread/demo2.java
@@ -0,0 +1,30 @@
+package Thread;
+
+class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("hello Thread");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+public class demo2 {
+    public static void main(String[] args) {
+        Thread t=new Thread(new MyRunnable());
+        t.start();
+        while (true) {
+            System.out.println("hello main");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
