46efcda073d0254cbd00fbfb0d804dfcfdb6b103
dcy <2284858868@qq.com>
2024-03-24 12:15:19 +0800

    线程创建方法3

---
+++ 初阶/Thread/Demo3.java
@@ -0,0 +1,29 @@
+package Thread;
+
+public class Demo3 {
+    public static void main(String[] args) {
+        new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    System.out.println("hello thread");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                }
+            }
+        }.start();
+
+        while (true) {
+            System.out.println("hello main");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
