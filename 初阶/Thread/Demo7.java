33cceb2199cf4d2044230b35aab38ac8ab3d21ea
dcy <2284858868@qq.com>
2024-03-24 12:41:11 +0800

    创建线程设为后台线程

---
+++ 初阶/Thread/Demo7.java
@@ -0,0 +1,21 @@
+package Thread;
+
+public class Demo7 {
+    public static void main(String[] args) {
+        Thread t=new Thread(()->{
+            for (int i = 0; i < 5; i++) {
+                System.out.println("hello Thread");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        t.setDaemon(true);
+
+        t.start();
+
+    }
+}
