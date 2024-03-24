3b749d26c6ad3246b6bca2f914253993e7ccb31f
dcy <2284858868@qq.com>
2024-03-24 12:19:49 +0800

    线程创建方法5

---
+++ 初阶/Thread/Demo5.java
@@ -0,0 +1,27 @@
+package Thread;
+
+public class Demo5 {
+    public static void main(String[] args) {
+        Thread t=new Thread(()->{
+            while (true) {
+                System.out.println("hello Thread");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        t.start();
+
+        while (true) {
+            System.out.println("hello main");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
