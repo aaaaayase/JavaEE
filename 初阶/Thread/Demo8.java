b86d2ddf3622d176cf6979dffbe827932931fcc7
dcy <2284858868@qq.com>
2024-03-24 12:44:35 +0800

    线程是否存活

---
+++ 初阶/Thread/Demo8.java
@@ -0,0 +1,21 @@
+package Thread;
+
+public class Demo8 {
+    public static void main(String[] args) {
+        Thread t=new Thread(()->{
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        t.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(t.isAlive());
+    }
+}
