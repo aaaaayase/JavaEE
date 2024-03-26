f311cf816d57fa3cb8717ed4c5a044830c832438
dcy <2284858868@qq.com>
2024-03-26 15:42:10 +0800

    线程安全的简单例子

---
+++ 初阶/Thread/Demo20.java
@@ -0,0 +1,40 @@
+package Thread;
+
+public class Demo20 {
+    static int count = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread t1 = new Thread(() -> {
+
+            for (int i = 0; i < 5000; i++) {
+                count++;
+
+            }
+
+        });
+
+        Thread t2= new Thread(() -> {
+
+            for (int i = 0; i < 5000; i++) {
+                count++;
+
+            }
+
+        });
+
+        //串行执行
+//        t1.start();
+//        t1.join();
+//        t2.start();
+//        t2.join();
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+        System.out.println("count:"+count);
+
+    }
+}
