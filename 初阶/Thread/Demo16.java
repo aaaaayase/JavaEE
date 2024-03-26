0de49f3cabfc7ee6c89c0eed9f1faf6a2bea0467
dcy <2284858868@qq.com>
2024-03-26 14:56:00 +0800

    sleep的时间是线程阻塞的时间，线程重新调度到cpu上还需要时间

---
+++ 初阶/Thread/Demo16.java
@@ -0,0 +1,13 @@
+package Thread;
+
+public class Demo16 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("beg:" + System.currentTimeMillis());
+        Thread.sleep(1000);
+        System.out.println("end:" + System.currentTimeMillis());
+
+
+    }
+}
