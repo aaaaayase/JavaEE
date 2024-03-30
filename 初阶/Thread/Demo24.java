bf11f9a8e23ff182b2861aa4df825c70ecc4127d
dcy <2284858868@qq.com>
2024-03-30 12:55:50 +0800

    线程安全问题解决-加锁2

---
+++ 初阶/Thread/Demo24.java
@@ -0,0 +1,61 @@
+package Thread;
+
+class Counter {
+    public int count = 0;
+
+    synchronized public void add() {
+//        synchronized (this) {
+//
+//        }
+        count++;
+    }
+
+    synchronized public static void func() {
+//        synchronized (Counter.class) {
+//
+//        }
+    }
+
+    public int get() {
+        return count;
+    }
+}
+
+
+public class Demo24 {
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+        //Counter counter1 = new Counter();
+        Thread t1 = new Thread(() -> {
+//            synchronized (counter) {
+//                counter.add();
+//            }
+            for (int i = 0; i < 5000; i++) {
+                counter.add();
+            }
+
+        });
+
+        Thread t2 = new Thread(() -> {
+//           synchronized (counter) {
+//               counter.add();
+//           }
+
+            //counter2.add();
+            for (int i = 0; i < 5000; i++) {
+                counter.add();
+            }
+        });
+
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        System.out.println(counter.get());
+    }
+
+}
