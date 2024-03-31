b1cf409641f8d882e185edf7e05d0922faeb89c2
dcy <2284858868@qq.com>
2024-03-31 18:07:13 +0800

    线程安全问题解决-等待通知1

---
+++ 初阶/Thread/Demo28.java
@@ -0,0 +1,19 @@
+package Thread;
+
+import java.util.Scanner;
+
+public class Demo28 {
+
+    public static void main(String[] args) throws InterruptedException {
+        Object locker = new Object();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("等待之前");
+        //wait必须在synchronized内使用，会解锁并且让线程进入阻塞等待状态
+        synchronized (locker) {
+            locker.wait();
+        }
+
+        System.out.println("等待之后");
+
+    }
+}
