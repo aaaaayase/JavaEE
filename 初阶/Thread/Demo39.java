d468febd489040702e6acc33c313bc708ddab4d9
dcy <2284858868@qq.com>
2024-04-15 22:28:10 +0800

    定时器的使用

---
+++ 初阶/Thread/Demo39.java
@@ -0,0 +1,29 @@
+package Thread;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Demo39 {
+
+    public static void main(String[] args) {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println(3333);
+            }
+        }, 3333);
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println(2222);
+            }
+        }, 2222);
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println(1111);
+            }
+        }, 1111);
+    }
+}
