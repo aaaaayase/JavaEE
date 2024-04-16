f84ca7f4a7ce51ab53ca66ab4b4d80936cc2245c
dcy <2284858868@qq.com>
2024-04-16 22:50:53 +0800

    原子类使用

---
+++ 初阶/Thread/Demo41.java
@@ -0,0 +1,40 @@
+package Thread;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Demo41 {
+    public static AtomicInteger count = new AtomicInteger(0);
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(() -> {
+            for (int i = 0; i < 50000; i++) {
+                //count++ 这里的对count的修改都是原子的
+                count.getAndIncrement();
+                //++count
+                //count.incrementAndGet();
+                //--count
+                //count.decrementAndGet();
+                //count--
+                //count.getAndDecrement();
+                //count+=10;
+                //count.getAndAdd(10);
+            }
+        });
+
+        Thread t1 = new Thread(() -> {
+
+            for (int i = 0; i < 50000; i++) {
+                count.getAndIncrement();
+            }
+        });
+
+        t.start();
+        t1.start();
+
+        t.join();
+        t1.join();
+        System.out.println(count);
+
+
+    }
+}
