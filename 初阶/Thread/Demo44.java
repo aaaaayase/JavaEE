d61a2f1033431fed858923b9ac101d0a096ebd54
dcy <2284858868@qq.com>
2024-04-17 18:49:27 +0800

    JUC中常用类-Semaphore申请及释放方法的基本使用

---
+++ 初阶/Thread/Demo44.java
@@ -0,0 +1,27 @@
+package Thread;
+
+import java.util.concurrent.Semaphore;
+
+public class Demo44 {
+
+    public static void main(String[] args) throws InterruptedException {
+        // 四个可用资源 P申请资源 V释放资源
+        Semaphore semaphore = new Semaphore(4);
+
+        semaphore.acquire(1);
+        System.out.println("P操作");
+        semaphore.acquire(1);
+        System.out.println("P操作");
+        semaphore.acquire(1);
+        System.out.println("P操作");
+        semaphore.acquire(1);
+        System.out.println("P操作");
+
+        // 此时信号量的四个资源已经被申请完了
+        // 如果继续申请的话就会堵塞 因为要等别的线程释放信号量的资源
+        semaphore.acquire();
+
+    }
+
+
+}
