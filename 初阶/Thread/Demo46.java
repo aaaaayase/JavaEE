65bbcd32eec43528ed50b665a0419b25fbcefe71
dcy <2284858868@qq.com>
2024-04-17 19:07:58 +0800

    JUC中常用类-CountDownLatch的使用

---
+++ 初阶/Thread/Demo46.java
@@ -0,0 +1,34 @@
+package Thread;
+
+import java.util.concurrent.CountDownLatch;
+
+public class Demo46 {
+
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(10);
+
+        for (int i = 0; i < 10; i++) {
+            int temp = i;
+            Thread t = new Thread(() -> {
+                System.out.println("线程启动:" + temp);
+                //当作任务
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("线程结束:" + temp);
+                latch.countDown();
+            });
+
+            t.start();
+
+        }
+
+        //等待所有线程中的任务结束
+        latch.await();
+        System.out.println("所有线程结束。");
+
+    }
+
+}
