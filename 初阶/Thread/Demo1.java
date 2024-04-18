e901057a141ff1e025fb20deec74f70c41a65a7f
dcy <2284858868@qq.com>
2024-04-18 23:35:45 +0800

    线程创建方法1

---
+++ 初阶/Thread/Demo1.java
@@ -0,0 +1,33 @@
+package Thread;
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("hello");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+public class Demo1 {
+    public static void main(String[] args) {
+        Thread t=new MyThread();
+        t.start();
+
+        while (true) {
+            System.out.println("hello main");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+}
